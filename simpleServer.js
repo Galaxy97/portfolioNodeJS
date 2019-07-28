901397e04871d2b4ee6c8615f58f7606a23cc596
Andrii Smahin <andrii.smahin@ekreative.com>
2019-07-28 10:32:19 +0300

    server v2

--- simpleServer.js
+++ simpleServer.js
@@ -2,7 +2,7 @@ const url = require('url')
 const http = require('http')
 const fs = require('fs')
 const queryString = require('querystring')
-const hostname = '192.168.31.120'
+const hostname = '127.0.0.1'
 const port = 3000
 
 const server = http.createServer((req, res) => {
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     if (hasQuery !== -1) {
       var query = queryString.parse(req.url.slice(hasQuery + 1)) // +1 необхідно бо вбудований в ноду парсер чутлий до "?"
     }
-    if (query.first && query.second) {
+    if (query.first) {
       fs.readFile('finility-097.json', 'utf8', (error, data) => {
         let newJSON = []
         if (error) {
@@ -22,41 +22,29 @@ const server = http.createServer((req, res) => {
         } else {
           data = JSON.parse(data)
           data = Object.values(data)
-          let first = query.first
-          let second = query.second
-          if(first > second){
-            second = query.first
-            first = query.second
+          let first = Number(query.first)
+          if (query.second) {
+            let second = Number(query.second)
+            if (first > second) {
+              second = query.first
+              first = query.second
+            }            
+            data.forEach(element => {
+              if (element['id'] >= first && element['id'] <= second) {
+                newJSON.push(element)
+              }
+            })
+          } else {
+            data.forEach(element => {
+              if (element['id'] <= first) {
+                newJSON.push(element)
+              }
+            })
           }
-          data.forEach(element => {
-            if(element['id'] >= first && element['id'] <= second ){
-              newJSON.push(element)
-            }
-          })
-          res.writeHead(200, { 'Content-Type': 'application/json' })
-          res.write(JSON.stringify(newJSON, null, 2))
-          res.end()
         }
-      })
-    } else if (query.first) {
-      fs.readFile('finility-097.json', 'utf8', (error, data) => {
-        let newJSON = []
-        if (error) {
-          res.writeHead(404)
-          res.write(error.toString())
-          res.end()
-        } else {
-          data = JSON.parse(data)
-          data = Object.values(data)
-          data.forEach(element => {
-            if( element['id'] <= query.first ){
-              newJSON.push(element)
-            }
-          })
-          res.writeHead(200, { 'Content-Type': 'application/json' })
-          res.write(JSON.stringify(newJSON, null, 2))
-          res.end()
-        }
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.write(JSON.stringify(newJSON, null, 2))
+        res.end()
       })
     }
   } else {
